a748c2d4cca3ee99afc3b1523a5e3cb0a09d9700
Maxym Goliak <devastium777@gmail.com>
2016-01-15 01:53:20 +0200

    Implementation of main window and git ignore config

--- poststore/poststore/App.xaml.cs
+++ poststore/poststore/App.xaml.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Windows;
-using System.Data;
-using System.Xml;
-using System.Configuration;
+﻿using System.Windows;
 
 namespace PostStore
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
-	{
-	}
+    public partial class App : Application
+    {
+    }
 }
--- poststore/poststore/ViewModels/MainVM.cs
+++ poststore/poststore/ViewModels/MainVM.cs
@@ -1,23 +1,64 @@
-﻿using System.Windows.Input;
+﻿using PostStore.Commands;
+using PostStore.Data.Models;
 using PostStore.Data.Repositories;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
 
 namespace PostStore.ViewModels
 {
     public class MainVM : VMBase
     {
+        #region Fields
+
         private RepositoryProvider provider;
+
+        #endregion
+
+        #region Constructor
 
         public MainVM()
         {
             provider = new RepositoryProvider();
+            CreateNewEntryCommand = new DelegateCommand(AddPostEntry);
+            Items = new ObservableCollection<PostEntryVM>(provider.PostEntries.All().Select(x => new PostEntryVM(x)));
         }
 
-        ICommand CreateNewEntryCommand
+        #endregion
+
+        #region Properties
+
+        public ICommand CreateNewEntryCommand
         {
-            get
-            {
-                return null;
-            }
+            get;
+            private set;
         }
+
+        public ObservableCollection<PostEntryVM> Items { get; private set; }
+
+        #endregion
+
+        #region Public methods
+
+        public void AddPostEntry()
+        {
+            var postEntry = new PostEntry
+            {
+                FirstName = "Maxym",
+                LastName = "Goliak",
+                ContainerID = "FUCK'N CONTAINER",
+                DateOfDischarge = DateTime.Now,
+                DateOfReport = DateTime.Now,
+                DateUplifted = DateTime.Now,
+                Balance = 100,
+                VesselName = "VESSEL",
+                TSDocId = 1
+            };
+            provider.PostEntries.AddNew(postEntry);
+            Items.Add(new PostEntryVM(postEntry));
+        }
+
+        #endregion
     }
 }
